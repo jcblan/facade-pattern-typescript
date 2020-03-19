4ba46ee4a9fc141d13843fd9555961f2d1654c4c
JcBlan <blancojulic@gmail.com>
2020-03-18 22:17:37 -0300

    Refactorizado stateNumero con set y get

--- src/AccionReceptor.ts
+++ src/AccionReceptor.ts
@@ -2,13 +2,20 @@
 
 export class AccionReceptor{
 
-    stateNumero: string;
+    private _stateNumero : string;
 
 
     constructor() {
         this.stateNumero = "";
     }
 
+    get stateNumero(): string {
+        return this._stateNumero;
+    }
+
+    set stateNumero(estado: string){
+        this._stateNumero = estado;
+    }
 
     ActionNumero(numero: string){
         this.stateNumero = this.stateNumero.concat(numero);
